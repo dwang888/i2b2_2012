746ddf8709a07a141e2e2b506d81db13f357bd94
Dong Wang <lingandcs@gmail.com>
2012-07-17 21:37:19 -0400

    initiate a project for i2b2 2012

---
+++ src/wd/i2b2/utilities/Token.java
@@ -0,0 +1,21 @@
+/**
+ * base class for tokens in data
+ */
+package wd.i2b2.utilities;
+
+public class Token {
+
+	String token;
+	String stem;
+	int startOffset;
+	int endOffset;
+	int length;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
